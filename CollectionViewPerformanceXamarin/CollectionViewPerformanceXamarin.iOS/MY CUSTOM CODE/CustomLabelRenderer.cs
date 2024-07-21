f74aaa32e2a6e5b12e5b9c5049a293ab0b397edb
jonmdev <43948450+jonmdev@users.noreply.github.com>
2024-07-21 17:31:54 -0400

    add ios test

---
+++ CollectionViewPerformanceXamarin/CollectionViewPerformanceXamarin.iOS/MY CUSTOM CODE/CustomLabelRenderer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using CollectionViewPerformanceXamarin;
+using CoreGraphics;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(Label), typeof(CustomAndroidLabelRenderer))]
+
+namespace CollectionViewPerformanceXamarin {
+
+    public class CustomAndroidLabelRenderer : LabelRenderer {
+
+        //https://devblogs.microsoft.com/xamarin/extending-xamarin-forms-controls-with-custom-renderers/
+        //https://stackoverflow.com/questions/55101691/custom-label-renderer-in-xamarin-forms-ondraw-never-fired
+
+        public CustomAndroidLabelRenderer() {
+            //this.AddOnLayoutChangeListener(new LayoutListener());
+            
+        }
+        public override void Draw(CGRect rect) {
+            base.Draw(rect);
+            UpdateCounter.addLayoutUpdate();
+        }
+        
+    }
+}
